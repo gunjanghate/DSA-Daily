95c96e98799fd716b87635ca5cc927fa496843a7
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-03-18 22:52:29 +0530

    Time: 11 ms (33.25%), Space: 144.6 MB (15.02%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,63 +1,40 @@
-// class Solution {
-// public:
-//     vector<int> rearrangeArray(vector<int>& nums) {
-//         ios_base::sync_with_stdio(false);
-//         cin.tie(NULL);
-//         vector<int> negs;
-//         vector<int> pos;
-//         vector<int> ans;
-//         for (auto i : nums) {
-//             if (i < 0)
-//                 negs.push_back(i);
-//             else
-//                 pos.push_back(i);
-//         }
-//         int i = 0;
-//         int j = 0;
-//         int turn = 0;
-//         while (i < pos.size() && j < negs.size()) {
-//             if (turn % 2 == 0) {
-//                 ans.push_back(pos[i]);
-//                 i++;
-//             } else {
-//                 ans.push_back(negs[j]);
-//                 j++;
-//             }
-//             turn++;
-//         }
-//         while(i<pos.size()){
-//                 ans.push_back(pos[i]);
-//                 i++;
-//         }
-//         while(j<negs.size()){
-//                 ans.push_back(negs[j]);
-//                 j++;
-//         }
-
-//         return ans;
-
-//     }
-// };
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        vector<int> ans(nums.size());
-        int i =0;
-        int j =0;
-        int k = 0;
-        while(k < nums.size()){
-            if(nums[k]>=0){
-                ans[2*j]=nums[k];
+        vector<int> negs;
+        vector<int> pos;
+        vector<int> ans;
+        for (auto i : nums) {
+            if (i < 0)
+                negs.push_back(i);
+            else
+                pos.push_back(i);
+        }
+        int i = 0;
+        int j = 0;
+        int turn = 0;
+        while (i < pos.size() && j < negs.size()) {
+            if (turn % 2 == 0) {
+                ans.push_back(pos[i]);
+                i++;
+            } else {
+                ans.push_back(negs[j]);
                 j++;
             }
-            else{
-                ans[2*i+1]=nums[k];
-                i++;
-            }
-                k++;
+            turn++;
         }
+        while(i<pos.size()){
+                ans.push_back(pos[i]);
+                i++;
+        }
+        while(j<negs.size()){
+                ans.push_back(negs[j]);
+                j++;
+        }
+
         return ans;
+
     }
 };
