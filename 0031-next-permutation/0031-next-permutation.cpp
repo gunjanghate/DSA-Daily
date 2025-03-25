75cf6d53354209d4b27932ac770fe21559a26d1a
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-03-25 23:12:23 +0530

    Time: 0 ms (100%), Space: 15.9 MB (5.13%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int ind = -1;
+        int n = nums.size();
+        for (int i = n - 2; i >= 0; i--) { //checking from back for breakpoint
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+        if (ind == -1) {
+            reverse(nums.begin(), nums.end()); //no breakpoint means next permuation is first sequence
+            return;
+        }
+        for (int i = n - 1; i >= ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin() + ind + 1, nums.end());
     }
 };
