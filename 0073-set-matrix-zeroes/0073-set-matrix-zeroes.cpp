d9c9ed95387d00865c7c5f6f915df9061ba603ff
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-03-29 00:21:23 +0530

    Time: 0 ms (100%), Space: 18.6 MB (79.28%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,22 +5,63 @@ public:
         cin.tie(NULL);
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> cols(m,0);
-        vector<int> row(n,0);
-        for(int i = 0; i<n; i++){
-            for(int j= 0; j<m; j++){
-                if(matrix[i][j]==0){
-                    cols[j]=1;
-                    row[i]=1;
+        // vector<int> cols(m,0);
+        // vector<int> row(n,0);
+        // for(int i = 0; i<n; i++){
+        //     for(int j= 0; j<m; j++){
+        //         if(matrix[i][j]==0){
+        //             cols[j]=1;
+        //             row[i]=1;
+        //         }
+        //     }
+        // }
+        // for(int i = 0; i<n; i++){
+        //     for(int j= 0; j<m; j++){
+        //         if(cols[j] || row[i]){
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
+         int col0 = 1;
+    // step 1: Traverse the matrix and
+    // mark 1st row & col accordingly:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                // mark i-th row:
+                matrix[i][0] = 0;
+
+                // mark j-th column:
+                if (j != 0)
+                    matrix[0][j] = 0;
+                else
+                    col0 = 0;
+            }
+        }
+    }
+
+    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (matrix[i][j] != 0) {
+                // check for col & row:
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
             }
         }
-        for(int i = 0; i<n; i++){
-            for(int j= 0; j<m; j++){
-                if(cols[j] || row[i]){
-                    matrix[i][j]=0;
-                }
-            }
+    }
+
+    //step 3: Finally mark the 1st col & then 1st row:
+    if (matrix[0][0] == 0) {
+        for (int j = 0; j < m; j++) {
+            matrix[0][j] = 0;
         }
+    }
+    if (col0 == 0) {
+        for (int i = 0; i < n; i++) {
+            matrix[i][0] = 0;
+        }
+    }
     }
 };
