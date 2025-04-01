33aa819c1616840174b52e4220bfbf9053c669de
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-04-01 22:31:19 +0530

    Time: 107 ms (5.22%), Space: 164.3 MB (5%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -7,13 +7,17 @@ public:
         if (memo.find(i) != memo.end())
             return memo[i];
 
-        long long take = questions[i][0] + solve(questions, i + questions[i][1] + 1);
+        long long take =
+            questions[i][0] + solve(questions, i + questions[i][1] + 1);
         long long skip = solve(questions, i + 1);
 
         return memo[i] = max(take, skip);
     }
 
     long long mostPoints(vector<vector<int>>& questions) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         return solve(questions, 0);
     }
 };
