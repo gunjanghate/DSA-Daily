37b1e744e29b4ae8c0e22cfaf45797a88e4f8978
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-04-01 22:30:50 +0530

    Time: 112 ms (5.22%), Space: 163.1 MB (5%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    unordered_map<int, long long> memo;
+    long long solve(vector<vector<int>>& questions, int i) {
+        if (i >= questions.size())
+            return 0;
+        if (memo.find(i) != memo.end())
+            return memo[i];
+
+        long long take = questions[i][0] + solve(questions, i + questions[i][1] + 1);
+        long long skip = solve(questions, i + 1);
+
+        return memo[i] = max(take, skip);
+    }
+
+    long long mostPoints(vector<vector<int>>& questions) {
+        return solve(questions, 0);
+    }
+};
