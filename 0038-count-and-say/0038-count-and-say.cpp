57b1fa2f8fbed44a437dd8c6737644056bd9316a
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-04-18 12:06:31 +0530

    Time: 5 ms (41.53%), Space: 10.1 MB (53.06%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -30,6 +30,9 @@
 class Solution {
 public:
     string countAndSay(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         string s = "1";
         for (int i = 2; i <= n; ++i) {
             string temp = "";
