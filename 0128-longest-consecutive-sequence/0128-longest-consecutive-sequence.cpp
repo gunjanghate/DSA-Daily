f59a6406efdb562c7e658c934353d7de1eb6629b
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-05-22 17:01:25 +0530

    Time: 86 ms (48.52%), Space: 88.5 MB (76.66%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -15,14 +15,13 @@ public:
         }
 
         for (auto it : st) {
-            // if 'it' is a starting number:
             if (st.find(it - 1) == st.end()) {
 
                 int cnt = 1;
                 int x = it;
                 while (st.find(x + 1) != st.end()) {
-                    x = x + 1;
-                    cnt = cnt + 1;
+                    x++;
+                    cnt++;
                 }
                 longest = max(longest, cnt);
             }
