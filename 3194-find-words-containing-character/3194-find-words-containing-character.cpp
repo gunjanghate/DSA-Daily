df77a8511421b8e472ba35b0493a17b58d5faf8e
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-05-24 11:09:23 +0530

    Time: 0 ms (100%), Space: 33.2 MB (21.58%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.cpp
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cpp
@@ -6,13 +6,17 @@ public:
 
         vector<int> ans;
         int l = words.size();
-        for(int w = 0; w < l; w++){
-            for(int c = 0; c < words[w].size(); c++){
-                if(words[w][c]==x){
-                    ans.push_back(w);
-                    break;
-                }
-            }
+        // for(int w = 0; w < l; w++){
+        //     for(int c = 0; c < words[w].size(); c++){
+        //         if(words[w][c]==x){
+        //             ans.push_back(w);
+        //             break;
+        //         }
+        //     }
+        // }
+         for(int i=0; i<l; i++){
+            if (words[i].find(x)!=-1)
+                ans.push_back(i);
         }
 
         return ans;
