65dc330eae03d1dcbf8c17c22b4dea4c9f79d7b1
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-04 17:57:21 +0530

    Time: 0 ms (100%), Space: 65 MB (11.08%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
@@ -3,35 +3,15 @@ public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        int  n = mat.size(), m= mat[0].size();
-        int cnt_max = 0;
-        int index = 0;
 
-        for (int i = 0; i < n; i++) {   
-            sort(mat[i].begin(), mat[i].end());    
-            int cnt_ones = m - lowerBound(mat[i], m, 1);
-            if (cnt_ones > cnt_max) {
-                cnt_max = cnt_ones;
-                index = i;
+        int row = 0, maxcnt = 0;
+        for(int i = 0, n = mat.size(); i < n; ++i){
+            int count = accumulate(mat[i].begin() , mat[i].end() , 0);
+            if(count > maxcnt){
+                row = i;
+                maxcnt = count;
             }
         }
-        return {index, cnt_max};
-    }
-    int lowerBound(vector<int> arr, int n, int x) {
-        int low = 0, high = n - 1;
-        int ans = n;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-           
-            if (arr[mid] >= x) {
-                ans = mid;
-                
-                high = mid - 1;
-            } else {
-                low = mid + 1; 
-            }
-        }
-        return ans;
+        return {row , maxcnt};
     }
 };
