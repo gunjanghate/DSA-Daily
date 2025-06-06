1123fd82886abb3be52453ffa7b4fa7bb84d7b2b
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-07 01:20:41 +0530

    Time: 15 ms (92.79%), Space: 62.7 MB (23.31%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,19 +1,35 @@
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         ios_base::sync_with_stdio(false);
+//         cin.tie(NULL);
+
+//         map <int, int> mp;
+//         for(auto i: nums) mp[i]++;
+
+//         int cnt = 0;
+//         for(auto it = mp.rbegin(); it != mp.rend(); ++it){
+//             cnt+=it->second;
+//             if(cnt>=k) return it->first;
+//         }
+
+//         return -1;
+        
+//     }
+// }; O(NLogN)
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-
-        map <int, int> mp;
-        for(auto i: nums) mp[i]++;
-
-        int cnt = 0;
-        for(auto it = mp.rbegin(); it != mp.rend(); ++it){
-            cnt+=it->second;
-            if(cnt>=k) return it->first;
-        }
-
-        return -1;
-        
+       priority_queue<int> pq;
+       k = nums.size() - k + 1;
+       for(int n : nums) {
+            if (pq.size() == k && pq.top() > n) {
+                pq.pop();
+            }
+            if (pq.size() < k) {
+                pq.push(n);
+            }
+       }
+    return pq.top();
     }
 };
