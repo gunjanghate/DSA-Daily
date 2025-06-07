7b32a9d59fa5d475205d959fe5a27d9650f687a0
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-08 02:12:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+int numberOfGasStationsRequired(long double dist, vector<int> &arr) {
+    int n = arr.size();
+    int cnt = 0;
+    for (int i = 1; i < n; i++) {
+        int numberInBetween = ((arr[i] - arr[i - 1]) / dist);
+        if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+            numberInBetween--;
+        }
+        cnt += numberInBetween;
+    }
+    return cnt;
+}
+
+double findSmallestMaxDist(vector<int> &arr, int k) {
+    int n = arr.size();
+    long double low = 0;
+    long double high = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        high = max(high, (long double)(arr[i + 1] - arr[i]));
+    }
+
+    long double diff = 1e-6 ;
+    while (high - low > diff) {
+        long double mid = (low + high) / (2.0);
+        int cnt = numberOfGasStationsRequired(mid, arr);
+        if (cnt > k) {
+            low = mid;
+        }
+        else {
+            high = mid;
+        }
+    }
+    return high;
+}
+};
