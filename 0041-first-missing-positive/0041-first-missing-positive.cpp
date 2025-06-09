7859c9cbcee6627baa1c81698312cb361ba6f690
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-09 15:26:08 +0530

    Time: 3 ms (55.35%), Space: 54.6 MB (99.86%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,20 +4,39 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        map<int, int> mp;
-        for(auto i : nums) mp[i]++;
-        if(!mp.count(1)){
-            return 1;
-        }
-        int ans = INT_MAX;
-        for(int i = 0;i<nums.size(); i++){
-            if(nums[i]<=0) continue;
-            if(nums[i] < INT_MAX && mp.count(nums[i]) != mp.count(nums[i]+1)){
-                ans = min(ans,nums[i]+1);
+        // map<int, int> mp;
+        // for(auto i : nums) mp[i]++;
+        // if(!mp.count(1)){
+        //     return 1;
+        // }
+        // int ans = INT_MAX;
+        // for(int i = 0;i<nums.size(); i++){
+        //     if(nums[i]<=0) continue;
+        //     if(nums[i] < INT_MAX && mp.count(nums[i]) !=
+        //     mp.count(nums[i]+1)){
+        //         ans = min(ans,nums[i]+1);
+        //     }
+        // }
+        // if(ans==-1) return nums.size();
+        // return ans;
+        // O(NLogN) :- TC
+        // O(N) :- SC
+        // using cyclic sort O(N) and O(1)
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] >= 1 && nums[i] <= n &&
+                   nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
             }
         }
-        if(ans==-1) return nums.size();
-        return ans;
-        
+
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
     }
 };
