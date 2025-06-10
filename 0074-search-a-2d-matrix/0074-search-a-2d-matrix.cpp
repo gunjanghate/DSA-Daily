d56aff63873538ad86ddc6ddc3b1319b16756ab1
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-10 19:52:10 +0530

    Time: 0 ms (100%), Space: 13.5 MB (7.31%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    // bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    //     ios_base::sync_with_stdio(false);
+    //     cin.tie(NULL);
+    //     int n = matrix.size();
+    //     int m = matrix[0].size();
+    //     for (int i = 0; i < n; i++) {
+    //         if (target >= matrix[i][0] && matrix[i][m-1]>=target) {
+    //             if (binary_search(matrix[i].begin(), matrix[i].end(), target))
+    //                 return true;
+    //         }
+    //     }
+
+    //     return false;
+    // }// O(N) + O(LogN)
+        bool searchMatrix(vector<vector<int>>& matrix, int target) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         int n = matrix.size();
         int m = matrix[0].size();
-        for (int i = 0; i < n; i++) {
-            if (target >= matrix[i][0] && matrix[i][m-1]>=target) {
-                if (binary_search(matrix[i].begin(), matrix[i].end(), target))
-                    return true;
-            }
+        int low = 0, high = n*m - 1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            //  | 0 1 2 3
+            //0 | 0 1 2 3
+            //1 | 4 5 6 7
+            //2 | 8 9 10 11
+            int rw = mid/m;
+            int cl = mid%m;
+            if(matrix[rw][cl]==target) return true;
+            if(matrix[rw][cl]>target) high = mid-1;
+            else low = mid+1;
         }
 
         return false;
