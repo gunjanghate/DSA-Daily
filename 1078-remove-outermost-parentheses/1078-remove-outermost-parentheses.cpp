0ff93325688216992e56c8126345711b4ab2fb17
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-17 23:20:02 +0530

    Time: 2 ms (25.65%), Space: 9.1 MB (24.5%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    string removeOuterParentheses(string s) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        string ans;
-        stack<char> st;
-        int sz = 0;
-        for (int i = 0; s[i] != '\0'; i++) {
-            if (s[i] == '(') {
-                if (sz == 0) {
-                    st.push(s[i]);
-                    sz++;
-                } else {
-                    st.push(s[i]);
-                    sz++;
-                    ans += s[i];
-                }
-            }
-
-            else if (s[i] == ')') {
-                if (sz > 1) {
-                    st.pop();
-                    sz--;
-                    ans += s[i];
-
-                } else {
-                    st.pop();
-                    sz--;
-                }
-            }
+string removeOuterParentheses(string s) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string ans;
+    int depth = 0;
+    for (char c : s) {
+        if (c == '(') {
+            if (depth > 0)
+                ans += c;
+            depth++;
+        } else {
+            depth--;
+            if (depth > 0)
+                ans += c;
         }
-
-        return ans;
     }
+    return ans;
+}
+
 };
