cad4eb28873e3834cb0b9116dc6c52b6b9ca53e2
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-18 11:25:10 +0530

    Time: 60 ms (26.79%), Space: 125.8 MB (23.68%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
@@ -1,10 +1,28 @@
 class Solution {
+private:
+    void countSort(vector<int>& arr) {
+        if (arr.empty())
+            return;
+        int maxVal = *max_element(arr.begin(), arr.end());
+        vector<int> count(maxVal + 1, 0);
+        for (int num : arr) {
+            count[num]++;
+        }
+        int index = 0;
+        for (int i = 0; i <= maxVal; i++) {
+            while (count[i]-- > 0) {
+                arr[index++] = i;
+            }
+        }
+    }
+
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        
-        sort(nums.begin(), nums.end());
+
+        // sort(nums.begin(), nums.end()); //NLogN
+        countSort(nums);
         // 1 1 3 3 4 5 7 8 9
         // 2 2 2 2 4 5
         // 2 2 2 4 5 5 5 5 7 7 8 8 9 9 10 11 12 12
