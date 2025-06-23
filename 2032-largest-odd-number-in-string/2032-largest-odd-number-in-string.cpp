ebea6a08d02b29b33011f9bd8d805e1a871ab5b9
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-23 15:15:09 +0530

    Time: 1 ms (44.21%), Space: 17.9 MB (18.53%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -3,27 +3,11 @@ public:
     string largestOddNumber(string num) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        // int l = num.length();
-        // int n = stoi(num);
-        // if(n%2==1) return to_string(n);
-        // while(n>0){
-        //     int f = n % 10;
-        //     int d = n/10;
-        //     if(f%2==1) return to_string(f);
-        //     if(d%2==1) return to_string(d);
-        //     n = n/10;
-        // }
-
-        // return ""; runtime error if input is very large
-                if (num.back() % 2 == 1)
-                    return num;
-                int i = num.length() - 1;
-                while (i >= 0) {
-                    int n = num[i];
-                    if (n % 2 == 1)
-                        return num.substr(0, i + 1);
-                    i--;
-                }
-                return "";
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if ((num[i] - '0') % 2 == 1) {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
     }
 };
