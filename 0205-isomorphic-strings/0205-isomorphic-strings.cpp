d6e2aefad2e020428f9346c100eb665e9b83b08b
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-23 16:40:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (80.42%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,13 +3,22 @@ public:
     bool isIsomorphic(string s, string t) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        unordered_map<char,int> mp;
-        unordered_map<char,int> mp1;
+        unordered_map<char, int> mp;
+        // unordered_map<char,int> mp1;
 
-        for(int i = 0; i<s.length(); i++){
-             if(mp.find(s[i]) == mp.end()) mp[s[i]]=i;
-             if(mp1.find(t[i]) == mp1.end()) mp1[t[i]]=i;
-             if(mp[s[i]]!=mp1[t[i]]) return false;
+        for (int i = 0; i < s.length(); i++) {
+            char a = s[i];
+            char b = t[i];
+            if (mp.count(a)) {
+
+                if (mp[a] != b)
+                    return false;
+            } else {
+                for (auto k : mp)
+                    if (k.second == b)
+                        return false;
+                mp[a] = b;
+            }
         }
 
         return true;
