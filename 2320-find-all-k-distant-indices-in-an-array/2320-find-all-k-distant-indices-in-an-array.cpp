4517631f9cb3938ab6a86a6285e16e5ce7b1e708
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-24 14:08:58 +0530

    Time: 16 ms (35.78%), Space: 14.9 MB (41.28%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
@@ -3,26 +3,27 @@ public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
+
         int n = nums.size();
-        vector<int> ans;
-        vector<int> indices;
-        for (int i = 0; i < n; i++)
-            if (nums[i] == key)
-                indices.push_back(i);
-        int n1 = indices.size();
-        if (n1 == n){
-            vector<int> ans1(n);
-            iota(ans1.begin(), ans1.end(), 0);
-            return ans1;
-        }
-        for(int i = 0; i<n ; i++){
-            for(int j = 0; j<n1 ; j++){
-                if(abs(i-indices[j])<=k && nums[indices[j]]==key){
-                    ans.push_back(i);
-                    break;
+        vector<bool> marked(n, false);
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == key) {
+                int start = max(0, i - k);
+                int end = min(n - 1, i + k);
+                for (int j = start; j <= end; j++) {
+                    marked[j] = true;
                 }
             }
         }
-        return ans;    
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (marked[i]) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
     }
 };
