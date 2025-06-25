94c7a23e2fedb206f33d88263e1612fd441d2788
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-25 15:45:22 +0530

    Time: 7 ms (22.36%), Space: 11.2 MB (49.35%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -3,19 +3,22 @@ public:
     string frequencySort(string s) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        string ans = "";
+        
         unordered_map<char, int> mp;
 
-        for (char c : s)
+        for (char c : s) {
             mp[c]++;
+        }
 
         vector<pair<char, int>> v(mp.begin(), mp.end());
-        sort(v.begin(), v.end(),
-             [](const auto& a, const auto& b) { return a.second > b.second; });
-        for (auto [i, k] : v) {
-            for (int j = 1; j <= k; j++) {
-                ans += i;
-            }
+
+        sort(v.begin(), v.end(), [](const auto& a, const auto& b) {
+            return a.second > b.second;
+        });
+
+        string ans = "";
+        for (auto [ch, freq] : v) {
+            ans.append(freq, ch);  
         }
 
         return ans;
