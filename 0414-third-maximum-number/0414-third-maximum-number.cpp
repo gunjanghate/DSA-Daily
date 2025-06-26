5b87a1b7311469529048de2d6e201fecd30c6a1a
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-06-26 21:33:53 +0530

    Time: 0 ms (100%), Space: 12.9 MB (56.08%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,28 +1,51 @@
+// class Solution {
+// public:
+//     int thirdMax(vector<int>& nums) {
+//         ios_base::sync_with_stdio(false);
+//         cin.tie(NULL);
+
+//         int fstmx = *max_element(nums.begin(), nums.end());
+//         int secmx = INT_MIN;
+//         for(auto i :nums){
+//             if(fstmx !=i && i>=secmx){
+//                 secmx = i;
+//             }
+//         }
+//         int thrdmx = INT_MIN;
+//         int f = 0;
+//         for(auto i :nums){
+//             if(secmx !=i && fstmx!=i && i>=thrdmx){
+//                 thrdmx = i;
+//                 f = 1;
+//             }
+//         }
+//         if(!f)
+//             thrdmx = fstmx;
+
+//         return thrdmx;
+
+//     }
+// };
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+        long long fst = LLONG_MIN, sec = LLONG_MIN, thrd = LLONG_MIN;
 
-        int fstmx = *max_element(nums.begin(), nums.end());
-        int secmx = INT_MIN;
-        for(auto i :nums){
-            if(fstmx !=i && i>=secmx){
-                secmx = i;
+        for (int num : nums) {
+            if (num == fst || num == sec || num == thrd) continue;
+
+            if (num > fst) {
+                thrd = sec;
+                sec = fst;
+                fst = num;
+            } else if (num > sec) {
+                thrd = sec;
+                sec = num;
+            } else if (num > thrd) {
+                thrd = num;
             }
         }
-        int thrdmx = INT_MIN;
-        int f = 0;
-        for(auto i :nums){
-            if(secmx !=i && fstmx!=i && i>=thrdmx){
-                thrdmx = i;
-                f = 1;
-            }
-        }
-        if(!f)
-            thrdmx = fstmx;
 
-        return thrdmx;
-
+        return thrd == LLONG_MIN ? fst : thrd;
     }
 };
