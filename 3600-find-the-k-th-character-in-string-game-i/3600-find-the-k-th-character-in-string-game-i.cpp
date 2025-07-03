8b21bf5aba7e2e329e741f8ce248db2e1a8d4893
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-07-03 17:36:40 +0530

    Time: 0 ms (100%), Space: 7.9 MB (85.56%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -4,22 +4,23 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        string s = "a";
-        if (k == 2)
-            return 'b';
+        // string s = "a";
+        // if (k == 2)
+        //     return 'b';
         
-        while (s.length() < k) {
-            string b = "";
-            for (int i = 0; i < s.length(); i++) {
-                if (s[i] == 'z') {
-                    b += 'a';
-                } else {
-                    b += char(s[i] + 1);
-                }
-            }
-            s += b;
-        }
+        // while (s.length() < k) {
+        //     string b = "";
+        //     for (int i = 0; i < s.length(); i++) {
+        //         if (s[i] == 'z') {
+        //             b += 'a';
+        //         } else {
+        //             b += char(s[i] + 1);
+        //         }
+        //     }
+        //     s += b;
+        // }
 
-        return s[k - 1]; 
+        // return s[k - 1]; 
+        return 'a' + popcount((uint)k-1);
     }
 };
