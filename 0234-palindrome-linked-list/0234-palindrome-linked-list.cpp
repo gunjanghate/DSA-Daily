b68bfd949473b6475c94c2bf9ecececc38f62d4f
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-07-08 22:16:27 +0530

    Time: 0 ms (100%), Space: 122.4 MB (43.6%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,22 +10,66 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        ListNode* temp = head;
-        stack<int> st;
-        while(temp!=NULL){
-            st.push(temp->val);
-            temp=temp->next;
-        }
+    // bool isPalindrome(ListNode* head) {
+    //     ListNode* temp = head;
+    //     stack<int> st;
+    //     while(temp!=NULL){
+    //         st.push(temp->val);
+    //         temp=temp->next;
+    //     }
 
-        ListNode* t = head;
-        while(t!=NULL){
-            if(t->val!=st.top()){
+    //     ListNode* t = head;
+    //     while(t!=NULL){
+    //         if(t->val!=st.top()){
+    //             return false;
+    //         }
+    //         st.pop();
+    //         t = t->next;
+    //     }
+
+    //     return true;
+    // } sc- O(n)
+    ListNode* reverse(ListNode* head) {
+        ListNode* rev = nullptr;
+        while (head != NULL) {
+            ListNode* temp = head->next;
+            head->next = rev;
+            rev = head;
+            head = temp;
+        }
+        return rev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        if (head->next == NULL) {
+            return true;
+        }
+        if (head->next->next == NULL) {
+            if (head->val == head->next->val)
+                return true;
+            return false;
+        }
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newHead = reverse(slow->next);
+        ListNode* second = newHead;
+        ListNode* first = head;
+        while (second != NULL && first != NULL) {
+            if (first->val != second->val) {
+                reverse(newHead);
                 return false;
             }
-            st.pop();
-            t = t->next;
+            first = first->next;
+            second = second->next;
         }
+        reverse(newHead);
 
         return true;
     }
