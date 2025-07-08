68cac317befaf918c9054281ca13d1d78b345954
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-07-08 20:07:13 +0530

    Time: 7 ms (68.58%), Space: 11.2 MB (98.64%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -7,21 +7,48 @@
  * };
  */
 class Solution {
-public:
-    ListNode *detectCycle(ListNode *head) {
+private:
+    bool hasCycle(ListNode* head) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        unordered_map<ListNode*, int>mp;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            if(mp.find(temp)!=mp.end()){
-                return temp;
+        if (head == nullptr || head->next == nullptr)
+            return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next->next;
+
+        while (slow != fast) {
+            if (fast == nullptr || fast->next == nullptr)
+                return false;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
+    }
+
+public:
+    ListNode* detectCycle(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+
+            slow = slow->next;
+
+            fast = fast->next->next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+
+                return slow;
             }
-            mp[temp]= 1;
-            temp = temp->next;
         }
 
         return NULL;
-
     }
 };
