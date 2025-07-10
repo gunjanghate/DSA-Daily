3576fefa80e6847390e263cb626959729b75ac13
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-07-10 23:31:35 +0530

    Time: 35 ms (70.32%), Space: 18.5 MB (81.67%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,23 +7,24 @@
  * };
  */
 class Solution {
-    private:
-    int size(ListNode* head){
+private:
+    int size(ListNode* head) {
         ListNode* temp = head;
         int s = 0;
-        while(temp){
+        while (temp) {
             s++;
             temp = temp->next;
         }
 
         return s;
     }
+
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        ListNode* temp1 = headA; 
-        ListNode* temp2 = headB;
+        // ListNode* temp1 = headA;
+        // ListNode* temp2 = headB;
         // unordered_map<ListNode*, int> mp;
         // while(temp2){
         //     mp[temp2] = 1;
@@ -36,34 +37,56 @@ public:
         //     temp1 = temp1->next;
         // }
 
-        // return NULL; 
-        int s1 = size(temp1);
-        int s2 = size(temp2);
-        int d;
-        if(s1>s2){
-            d = s1-s2;
-        }else{
-            d = s2-s1;
+        // return NULL;
+        /////////////////////////////////
+        // int s1 = size(temp1);
+        // int s2 = size(temp2);
+        // int d;
+        // if(s1>s2){
+        //     d = s1-s2;
+        // }else{
+        //     d = s2-s1;
+        // }
+        // while(d){
+        //     if(s1>s2){
+        //         temp1 = temp1->next;
+        //         d--;
+        //     }else{
+        //         temp2 = temp2->next;
+        //         d--;
+        //     }
+        // }
+        // while(temp1 && temp2){
+        //     if(temp1 == temp2){
+        //         return temp1;
+        //     }
+        //     temp1 = temp1->next;
+        //     temp2 = temp2->next;
+        // }
+
+        // return NULL;
+        ////////////////////////////////
+        ListNode* d1 = headA;
+        ListNode* d2 = headB;
+        if(!d1 || !d2){
+            return NULL;
         }
-        while(d){
-            if(s1>s2){
-                temp1 = temp1->next;
-                d--;
-            }else{
-                temp2 = temp2->next;
-                d--;
-            } 
-        }
-        while(temp1 && temp2){
-            if(temp1 == temp2){
-                return temp1;
+
+        while (d1 != d2) {
+            d1 = d1->next;
+            d2 = d2->next;
+            if(d1==d2){
+                return d1;
             }
-            temp1 = temp1->next;
-            temp2 = temp2->next;
+            if(d1==nullptr){
+                d1 = headB;
+            }
+            if(d2==nullptr){
+                d2 = headA;
+            }
+           
         }
 
-        return NULL;
-
-        }
-            
+        return d1;
+    }
 };
