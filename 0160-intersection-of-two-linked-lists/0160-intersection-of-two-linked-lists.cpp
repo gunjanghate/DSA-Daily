b6d688c4713f424fe35efe5f48c2a6c7791f626e
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-07-10 23:12:13 +0530

    Time: 38 ms (49.48%), Space: 18.5 MB (52.88%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,24 +7,63 @@
  * };
  */
 class Solution {
+    private:
+    int size(ListNode* head){
+        ListNode* temp = head;
+        int s = 0;
+        while(temp){
+            s++;
+            temp = temp->next;
+        }
+
+        return s;
+    }
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         ListNode* temp1 = headA; 
         ListNode* temp2 = headB;
-        unordered_map<ListNode*, int> mp;
-        while(temp2){
-            mp[temp2] = 1;
-            temp2 = temp2->next;
+        // unordered_map<ListNode*, int> mp;
+        // while(temp2){
+        //     mp[temp2] = 1;
+        //     temp2 = temp2->next;
+        // }
+        // while(temp1){
+        //     if(mp[temp1]){
+        //         return temp1;
+        //     }
+        //     temp1 = temp1->next;
+        // }
+
+        // return NULL; 
+        int s1 = size(temp1);
+        int s2 = size(temp2);
+        int d;
+        if(s1>s2){
+            d = s1-s2;
+        }else{
+            d = s2-s1;
         }
-        while(temp1){
-            if(mp[temp1]){
+        while(d){
+            if(s1>s2){
+                temp1 = temp1->next;
+                d--;
+            }else{
+                temp2 = temp2->next;
+                d--;
+            } 
+        }
+        while(temp1 && temp2){
+            if(temp1 == temp2){
                 return temp1;
             }
             temp1 = temp1->next;
+            temp2 = temp2->next;
         }
 
-        return NULL; 
-    }
+        return NULL;
+
+        }
+            
 };
