f80c45894bbf850e3a2b00537ae0d4bed090c361
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-07-11 00:16:22 +0530

    Time: 0 ms (100%), Space: 77.2 MB (43.06%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,65 +10,77 @@
  */
 class Solution {
 private:
-    int sizeOfLL(ListNode* head) {
-        ListNode* temp = head;
-        int size = 0;
-        while (temp != NULL) {
-            size++;
-            temp = temp->next;
-        }
+    // // Get size of linked list
+    // int sizeOfLL(ListNode* head) {
+    //     int size = 0;
+    //     while (head) {
+    //         size++;
+    //         head = head->next;
+    //     }
+    //     return size;
+    // }
 
-        return size;
-    }
-    ListNode* appendZeros(ListNode* list, int diff) {
-
-        ListNode* temp = list;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        while (diff--) {
-            temp->next = new ListNode(0);
-            temp = temp->next;
-        }
-        return list;
-    }
+    // // Append 'diff' number of 0s at the end of the list
+    // ListNode* appendZeros(ListNode* list, int diff) {
+    //     ListNode* temp = list;
+    //     while (temp->next) temp = temp->next;
+    //     while (diff--) {
+    //         temp->next = new ListNode(0);
+    //         temp = temp->next;
+    //     }
+    //     return list;
+    // }
 
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        int n1 = sizeOfLL(l1);
-        int n2 = sizeOfLL(l2);
-        if (n1 > n2) {
-            l2 = appendZeros(l2, n1 - n2);
-        } else if (n2 > n1) {
-            l1 = appendZeros(l1, n2 - n1);
-        }
+        // int n1 = sizeOfLL(l1);
+        // int n2 = sizeOfLL(l2);
+        // if (n1 > n2) l2 = appendZeros(l2, n1 - n2);
+        // else if (n2 > n1) l1 = appendZeros(l1, n2 - n1);
 
-        ListNode* ans = n1>n2 ? l1 : l2;
-        ListNode* res = ans;
-        ListNode* prevRes = ans;
-        ListNode* t1 = l1;
-        ListNode* t2 = l2;
+        // ListNode* ans = new ListNode(0); 
+        // ListNode* res = ans;
+        // int carry = 0;
+
+        // while (l1 && l2) {
+        //     int sum = l1->val + l2->val + carry;
+        //     carry = sum / 10;
+        //     res->next = new ListNode(sum % 10);
+        //     res = res->next;
+        //     l1 = l1->next;
+        //     l2 = l2->next;
+        // }
+
+        // if (carry)
+        //     res->next = new ListNode(carry);
+
+        // return ans->next;  // skip dummy node
+
+        ListNode *dummy = new ListNode(); 
+        ListNode *temp = dummy; 
         int carry = 0;
-        while (t1 != NULL && t2 != NULL) {
-            int s = t1->val + t2->val + carry;
-            if (s > 9)
-                carry = 1;
-            else
-                carry = 0;
-            res->val = s % 10;
-            prevRes = res;
-            res = res->next;
-            t1 = t1->next;
-            t2 = t2->next;
+        while( (l1 != NULL || l2 != NULL) || carry) {
+            int sum = 0; 
+            if(l1 != NULL) {
+                sum += l1->val; 
+                l1 = l1 -> next; 
+            }
+            
+            if(l2 != NULL) {
+                sum += l2 -> val; 
+                l2 = l2 -> next; 
+            }
+            
+            sum += carry; 
+            carry = sum / 10; 
+            ListNode *node = new ListNode(sum % 10); 
+            temp -> next = node; 
+            temp = temp -> next; 
         }
-        if(carry){
-            ListNode* carryNode = new ListNode(carry);
-            prevRes->next = carryNode;
-        }
+        return dummy -> next; 
 
-        return ans;
     }
 };
