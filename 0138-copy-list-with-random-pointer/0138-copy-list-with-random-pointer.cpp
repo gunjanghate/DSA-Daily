435d391e5ffc83660fe76c66efdc1559ed21d83d
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-07-16 17:58:09 +0530

    Time: 5 ms (64.83%), Space: 15.3 MB (26.36%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -5,7 +5,7 @@ public:
     int val;
     Node* next;
     Node* random;
-    
+
     Node(int _val) {
         val = _val;
         next = NULL;
@@ -16,24 +16,80 @@ public:
 
 class Solution {
 public:
-    Node* copyRandomList(Node* head) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+    void insertCopyInBetween(Node* head) {
         Node* temp = head;
-        if(!head) return NULL;
-        unordered_map<Node*, Node*> mpp;
-        while(temp){
-            Node* newNode = new Node(temp->val);
-            mpp[temp] = newNode;
-            temp = temp->next;
-        }
-        temp = head;
-        while(temp){
-            mpp[temp]->next = mpp[temp->next];
-            mpp[temp]->random = mpp[temp->random];
-            temp = temp->next;
-        }
+        while (temp != NULL) {
+            Node* nextElement = temp->next;
+            // Create a new node with the same data
+            Node* copy = new Node(temp->val);
 
-        return mpp[head];
+            // Point the copy's next to
+            // the original node's next
+            copy->next = nextElement;
+
+            // Point the original
+            // node's next to the copy
+            temp->next = copy;
+
+            // Move to the next original node
+            temp = nextElement;
+        }
     }
+
+    // Function to connect random
+    // pointers of the copied nodes
+    void connectRandomPointers(Node* head) {
+        Node* temp = head;
+        while (temp != NULL) {
+            // Access the copied node
+            Node* copyNode = temp->next;
+
+            // If the original node
+            // has a random pointer
+            if (temp->random) {
+                // Point the copied node's random to the
+                // corresponding copied random node
+                copyNode->random = temp->random->next;
+            } else {
+                // Set the copied node's random to
+                // null if the original random is null
+                copyNode->random = NULL;
+            }
+
+            // Move to the next original node
+            temp = temp->next->next;
+        }
+    }
+
+    // Function to retrieve the
+    // deep copy of the linked list
+
+Node* copyRandomList(Node* head) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!head) return nullptr;
+
+    // Step 1: Insert copied nodes in between
+    insertCopyInBetween(head);
+
+    // Step 2: Connect the random pointers
+    connectRandomPointers(head);
+
+    // Step 3: Separate the copied list
+    Node* temp = head;
+    Node* dummyNode = new Node(-1);
+    Node* copyTail = dummyNode;
+
+    while (temp) {
+        Node* copyNode = temp->next;
+        copyTail->next = copyNode;
+        copyTail = copyNode;
+
+        temp->next = copyNode->next; // Restore original list
+        temp = temp->next;
+    }
+
+    return dummyNode->next;
+}
+
 };
