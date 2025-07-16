a30c9ccb9b1487b75e324d0c4878157ff355484a
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-07-16 13:53:41 +0530

    Time: 4 ms (65.29%), Space: 96.2 MB (62.35%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -3,21 +3,18 @@ public:
     int maximumLength(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        
         int n = nums.size();
-        if(n==2) return 2;
-        int c1 = 0, c2 = 0, eve = 0, odd = 0;
-        for(auto i: nums){
-            if(i%2==0){
-                c1++;
-                eve = max(eve, odd+1);
-            }else{
-                c2++;
-                odd = max(odd, eve+1);
+
+        int odd = 0, even = 0, alternate = 1, prev = nums[0];
+        for(int i = 0; i < n; i++){
+            if(nums[i] % 2 == 1) odd++;
+            else even++;
+
+            if(i >= 1 && nums[i] % 2 != prev % 2){
+                alternate++;
+                prev = nums[i];
             }
         }
-
-        return max({c1, c2, eve, odd});
-
+        return max({odd, even, alternate});
     }
 };
