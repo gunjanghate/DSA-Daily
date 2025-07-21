c4691999141b43c961f27c503e55b5545f42ce82
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-07-21 19:09:40 +0530

    Time: 4 ms (98.8%), Space: 35.2 MB (100%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    string makeFancyString(string s) {
+    string makeFancyString(string& s) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        
-        int c = 1;
-        string ans = "";
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (ans.length() == 0) {
-                ans += s[i];
-                continue;
-            }
-            
-            if (ans.back() == s[i]) {
-                if (c < 2) {
-                    ans += s[i];
-                    c++;
-                }
-            } else {
-                ans += s[i];
-                c = 1;
-            }
+        int write = 1, count = 1;
+
+        for (int read = 1; read < s.size(); ++read) {
+            if (s[read] == s[read - 1])
+                count++;
+            else
+                count = 1;
+
+            if (count < 3)
+                s[write++] = s[read];
         }
 
-        return ans;
+        s.resize(write);
+        return s;
     }
 };
