cfee9f70b61aac82a1caab7efb881fcc33f38ffc
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-08-05 20:33:02 +0530

    Time: 0 ms (100%), Space: 48.9 MB (75.84%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
         int ans = 0;
-        int f = 0;
-        for(int i = 0; i<fruits.size(); i++){
-            f = 0;
-            for(int j = 0; j<baskets.size(); j++){
-                if(baskets[j]==-1) continue;
-                if(fruits[i]<=baskets[j]){
-                    f = 1;
-                    baskets[j] = -1;
+
+        for (int fruit : fruits) {
+            bool placed = false;
+            for (int &basket : baskets) {
+                if (basket != -1 && basket >= fruit) {
+                    basket = -1;   
+                    placed = true;
                     break;
                 }
             }
-            if(!f) ans++;
+            if (!placed) ans++;
         }
-
         return ans;
     }
 };
