147befb5ecd0053c99a7a29c7a0dc2fc56922efb
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-08-06 23:13:17 +0530

    Time: 0 ms (100%), Space: 17.9 MB (79.73%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,22 +4,19 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        unordered_map<int, int> freq;
-        for (auto n : nums) freq[n]++;
+        unordered_map<int,int> freq;
+        for (int n : nums) freq[n]++;
 
-        vector<int> sortedFreq;
-        for (auto &p : freq) sortedFreq.push_back(p.second);
-        sort(sortedFreq.begin(), sortedFreq.end(), greater<int>());
-
-        vector<int> kfreqs(sortedFreq.begin(), sortedFreq.begin() + k);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+        for (auto &p : freq) {
+            pq.push({p.second, p.first});
+            if (pq.size() > k) pq.pop();
+        }
 
         vector<int> ans;
-        for (auto &p : freq) {
-            if (find(kfreqs.begin(), kfreqs.end(), p.second) != kfreqs.end()) {
-                ans.push_back(p.first);
-                kfreqs.erase(find(kfreqs.begin(), kfreqs.end(), p.second));
-                if (ans.size() == k) break;
-            }
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
         return ans;
     }
