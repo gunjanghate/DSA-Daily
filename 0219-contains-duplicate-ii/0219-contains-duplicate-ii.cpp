ac394166a43351e1ee2f72a902d212d6fb179a2f
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-09-28 19:45:06 +0530

    Time: 62 ms (97.22%), Space: 98.7 MB (53.97%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.count(nums[i]) > 0) {
+                if (k >= abs(mp[nums[i]] - i)) {
+                    return true;
+                }
+            }
+            mp[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
