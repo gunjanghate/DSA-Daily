9a4e940e76f17bdd76c72b49c2dcb00f07c14c60
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-11-13 11:24:01 +0530

    Time: 3 ms (83.33%), Space: 15.8 MB (75%) - LeetHub

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long maxOperations(string s) {
+        long long cnt = 0;
+        long long ones = (s[0] == '1');
+        int n = s.size();
+
+        for (int i = 1; i < n; i++) {
+            if (s[i] == '1') {
+                ones++;
+            } else if (s[i - 1] == '1') {
+                cnt += ones;
+            }
+        }
+
+        return cnt;
+    }
+};
