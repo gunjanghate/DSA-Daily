b56a60027a1cc762ee0af9990ec027b6bbbcd8f1
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-12-04 14:31:56 +0530

    Time: 58 ms (12.46%), Space: 23.8 MB (6.96%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int countCollisions(string d) {
+        stack<char> s;
+        int collisions = 0;
+
+        for (char c : d) {
+            // Case 1: car is L → may cause multiple collisions
+            if (c == 'L') {
+                if (!s.empty() && s.top() == 'R') {
+                    collisions += 2;
+                    s.pop();
+                    while (!s.empty() && s.top() == 'R') {
+                        collisions++;
+                        s.pop();
+                    }
+
+                    s.push('S');
+                }
+                else if (!s.empty() && s.top() == 'S') {
+                    collisions++;
+                    s.push('S');
+                }
+                else {
+                    s.push('L');
+                }
+            }
+
+            // Case 2: car is R → just push it
+            else if (c == 'R') {
+                s.push('R');
+            }
+
+            // Case 3: car is S → may absorb R's
+            else if (c == 'S') {
+                while (!s.empty() && s.top() == 'R') {
+                    collisions++;
+                    s.pop();
+                }
+
+                s.push('S');
+            }
+        }
+
+        return collisions;
+    }
+};
