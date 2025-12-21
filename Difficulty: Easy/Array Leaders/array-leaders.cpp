78102c655b52049cc7fffa98f66ad2315c52d523
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-12-21 15:15:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.cpp
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -1,10 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 
 
 class Solution {
@@ -12,56 +5,19 @@ class Solution {
   public:
     vector<int> leaders(vector<int>& arr) {
         // Code here
-        int mx = INT_MIN;
         vector<int> ans;
         if(arr.size()==0) return ans;
+        int mx = -1;
+        
         for(int i = arr.size()-1; i>=0; i--){
-            if(arr[i]>=mx){
+            if(arr[i] >= mx){
                 mx = arr[i];
-                ans.push_back(mx);
+                ans.push_back(arr[i]);
             }
         }
+        
         reverse(ans.begin(), ans.end());
+        
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t; // Number of test cases
-    cin >> t;
-    cin.ignore(); // Ignore the newline after the integer input
-    while (t--) {
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num); // Read the array elements from input string
-
-        Solution obj;
-        vector<int> result = obj.leaders(a);
-
-        // Print the result in the required format
-        if (result.empty()) {
-            cout << "[]"; // Print empty brackets if no leaders are found
-        } else {
-            for (int i = 0; i < result.size(); i++) {
-                if (i != 0)
-                    cout << " ";
-                cout << result[i];
-            }
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
