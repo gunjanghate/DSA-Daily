7a4891908f97d43264afa336b84b5cfaa170346e
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2025-12-23 11:23:29 +0530

    Time: 0 ms (100%), Space: 14.8 MB (8.36%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int> s;
+        unordered_set<int> s;
         vector<int> ans;
-        map<int, int> m1;
+        unordered_map<int, int> m1;
         for(int i : nums1) m1[i]++;
 
         for(int i : nums2){
