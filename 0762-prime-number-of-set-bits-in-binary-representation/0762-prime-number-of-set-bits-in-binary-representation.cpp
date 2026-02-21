4624b17ee03ecb5326c6cc8391390e8888a3e4a2
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-02-21 21:48:14 +0530

    Time: 2 ms (77.88%), Space: 8 MB (35.12%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -3,14 +3,9 @@ public:
     bool isPrime(long long n) {
         if (n <= 1)
             return false;
-        if (n <= 3)
-            return true;
 
-        if (n % 2 == 0 || n % 3 == 0)
-            return false;
-
-        for (long long i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0)
+        for (long long i = 2; i * i <= n; i++) {
+            if (n % i == 0)
                 return false;
         }
 
@@ -21,8 +16,8 @@ public:
         int ans = 0;
         for (int i = left; i <= right; i++) {
             int a = __builtin_popcount(i);
-            if (isPrime(a)) ans++;
-
+            if (isPrime(a))
+                ans++;
         }
 
         return ans;
