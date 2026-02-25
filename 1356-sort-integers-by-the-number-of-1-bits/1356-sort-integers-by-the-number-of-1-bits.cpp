6dff619cb73b2c8b20b90ed09a79d12237027f69
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-02-25 14:43:05 +0530

    Time: 3 ms (47.07%), Space: 14.2 MB (58.88%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        map<int, vector<int>> mp;
-        vector<int> ans;
-        for (auto i : arr) {
-            mp[__builtin_popcount(i)].push_back(i);
-        }
-
-        for (auto m : mp) {
-            sort(m.second.begin(), m.second.end());
-            for (int i = 0; i < m.second.size(); i++) {
-                ans.push_back(m.second[i]);
-            }
-        }
-
-        return ans;
+        sort(arr.begin(),arr.end() , [] (int a, int b){
+        int countA=__builtin_popcount(a);
+        int countB=__builtin_popcount(b);
+        if(countA==countB) return a<b;
+        return countA<countB;
+        });
+        return arr;
+        
     }
 };
