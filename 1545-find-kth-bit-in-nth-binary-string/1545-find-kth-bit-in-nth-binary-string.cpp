a8b40a673e384bbfe685a7c02f27fa3a9bd0f8ec
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-03-03 18:58:37 +0530

    Time: 0 ms (100%), Space: 7.9 MB (63.07%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    string invertStr(string s) {
-        for (char& c : s)
-            c = (c == '0') ? '1' : '0';
-        return s;
-    }
     char findKthBit(int n, int k) {
-        // vector<string> s(n);
-        string last = "0";
+        
+        if (n == 1)
+            return '0';
 
-        for (int i = 1; i < n; i++) {
-            string firstPart = last;
-            string secondPart = invertStr(last);
-            reverse(secondPart.begin(), secondPart.end());
-            string s = firstPart + '1' + secondPart;
-            last = s;
-        }
+        int len = (1 << n) - 1;
+        int mid = (len + 1) / 2;
 
-        return last[k-1];
+        if (k == mid)
+            return '1';
+
+        if (k < mid)
+            return findKthBit(n - 1, k);
+
+    
+        char bit = findKthBit(n - 1, len - k + 1);
+        return (bit == '0') ? '1' : '0';
     }
 };
