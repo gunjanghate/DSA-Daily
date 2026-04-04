51c16a8c7845e088d82ad596752770382685daaf
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-04-04 07:48:24 +0530

    Time: 15 ms (69.61%), Space: 25 MB (78.38%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        unordered_map<string, vector<string>> res;
+        unordered_map<string, vector<string>> mp;
 
-        for (string& s : strs) {
-            vector<int> count(26, 0);
-
-            for (char& c : s) {
-                count[c - 'a']++;
-            }
-            string k;
-            for (int i = 0; i < 26; i++) {
-                k += '#' + to_string(count[i]);
-            }
-            res[k].push_back(s);
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end()); 
+            mp[key].push_back(s);
         }
 
+       
         vector<vector<string>> result;
-        for (auto& [key, grp] : res) {
-            result.push_back(grp);
+        for (auto &it : mp) {
+            result.push_back(it.second);
         }
         return result;
     }
