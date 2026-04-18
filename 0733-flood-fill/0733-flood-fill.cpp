309f1e5dd3dba07c0228fd480bc291dc205eaaea
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-04-18 19:10:08 +0530

    Time: 0 ms (100%), Space: 18 MB (89.31%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     void dfs(int initColor, int r, int c, vector<vector<int>>& ans,
-             vector<vector<int>>& image, int color, int dr[], int dc[]) {
+            int color, int dr[], int dc[]) {
+    // void dfs(int initColor, int r, int c, vector<vector<int>>& ans,
+    //          vector<vector<int>>& image, int color, int dr[], int dc[]) {
         ans[r][c] = color;
-        int n = image.size();
-        int m = image[0].size();
+        int n = ans.size();
+        int m = ans[0].size();
 
         for (int i = 0; i < 4; i++) {
             int nr = r + dr[i];
             int nc = c + dc[i];
 
             if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
-                image[nr][nc] == initColor && ans[nr][nc] != color) {
-                    dfs(initColor, nr, nc, ans, image, color, dr, dc);
+                ans[nr][nc] == initColor && ans[nr][nc] != color) {
+                    // dfs(initColor, nr, nc, ans, image, color, dr, dc);
+                    dfs(initColor, nr, nc, ans, color, dr, dc);
             }
         }
     }
@@ -24,7 +27,7 @@ public:
 
         int dr[] = {-1, 0, 1, 0};
         int dc[] = {0, -1, 0, 1};
-        dfs(initColor, sr, sc, ans, image, color, dr, dc);
+        dfs(initColor, sr, sc, ans, color, dr, dc);
     
         return ans;
     }
