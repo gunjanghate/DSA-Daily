d6a9940ded282da055b39b75151c50234f2cf29f
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-04-28 00:58:35 +0530

    Time: 7 ms (61.31%), Space: 15.3 MB (29.03%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,65 +1,45 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int calculate(string s) {
-        int ans = 0;
         stack<int> st;
-        int i = 0;
-        int n = s.length();
+        int num = 0;
+        char op = '+';
 
-        while (i < n) {
+        for(int i = 0; i < s.size(); i++) {
+            char c = s[i];
 
-            if (s[i] == ' ') {
-                i++;
-                continue;
+            if(isdigit(c)) {
+                num = num * 10 + (c - '0');
             }
 
-            if (isdigit(s[i])) {
-                int num = 0;
-                while (i < n && isdigit(s[i])) {
-                    num = num * 10 + (s[i] - '0');
-                    i++;
-                }
-
-                // check for * or /
-                if (!st.empty() && (st.top() == '*' || st.top() == '/')) {
-                    char op = st.top(); st.pop();
-                    int n1 = st.top(); st.pop();
-
-                    if (op == '*') st.push(n1 * num);
-                    else st.push(n1 / num);
-                } else {
+            if((!isdigit(c) && c != ' ') || i == s.size() - 1) {
+                if(op == '+') {
                     st.push(num);
                 }
+                else if(op == '-') {
+                    st.push(-num);
+                }
+                else if(op == '*') {
+                    int top = st.top(); st.pop();
+                    st.push(top * num);
+                }
+                else if(op == '/') {
+                    int top = st.top(); st.pop();
+                    st.push(top / num);
+                }
 
-                continue;
+                op = c;
+                num = 0;
             }
-
-            st.push(s[i]);
-            i++;
         }
 
-        // second pass for + and -
-        stack<int> st2;
 
-        while (!st.empty()) {
-            st2.push(st.top());
+        int result = 0;
+        while(!st.empty()) {
+            result += st.top();
             st.pop();
         }
 
-        ans = st2.top();
-        st2.pop();
-
-        while (!st2.empty()) {
-            char op = st2.top(); st2.pop();
-            int num = st2.top(); st2.pop();
-
-            if (op == '+') ans += num;
-            else if (op == '-') ans -= num;
-        }
-
-        return ans;
+        return result;
     }
 };
