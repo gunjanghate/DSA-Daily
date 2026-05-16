3a61a5a86e53c54321fda5fa3a75f9f271ba4d9b
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-05-16 15:53:19 +0530

    Time: 0 ms (100%), Space: 16.1 MB (90%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -4,28 +4,6 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        int n = arr.size();
-        int l = 0, h = n - 1;
-        int m;
-        int ans = INT_MAX;
-        while (l <= h) {
-            m = l + (h - l) / 2;
-            if (arr[l] < arr[h]) {
-                ans = min(ans, arr[l]);
-                break;
-            }
-            if (arr[l] == arr[h]) {
-                ans = min(ans, arr[l]);
-                l++;
-            }
-            else if (arr[m] >= arr[l]) {
-                ans = min(ans, arr[h]);
-                l = m + 1;
-            }else {
-                ans = min(ans, arr[m]);
-                h = m - 1;
-            }
-        }
-        return ans;
+        return *min_element(arr.begin(), arr.end());
     }
 };
