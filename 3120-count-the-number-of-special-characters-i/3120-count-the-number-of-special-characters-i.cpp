389cc50ff9dfeb184fb9777859710b57b364c525
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-05-26 08:50:56 +0530

    Time: 3 ms (29%), Space: 9.4 MB (35.03%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
+
         unordered_set<char> st;
         int ans = 0;
-        for(int i = 0; i<word.length(); i++){
-            char c  = word[i];
-            if(c>='a' && c<='z'){
-                if(st.find(toupper(c))!=st.end() && st.find(c)==st.end()){
-                    st.insert(c);
+
+        for(char c : word){
+
+            if(islower(c)){
+
+                if(st.find(toupper(c)) != st.end() &&
+                   st.find(c) == st.end()) {
+
                     ans++;
-                }else{
-                    st.insert(c);
                 }
-            }else if(c>='A' && c<='Z'){
-                if(st.find(tolower(c))!=st.end() && st.find(c)==st.end()){
-                    st.insert(c);
+
+                st.insert(c);
+            }
+
+            else if(isupper(c)){
+
+                if(st.find(tolower(c)) != st.end() &&
+                   st.find(c) == st.end()) {
+
                     ans++;
-                }else{
-                    st.insert(c);
                 }
+
+                st.insert(c);
             }
         }
 
