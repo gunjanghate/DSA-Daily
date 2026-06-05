c1d684bd5b5b84eef70f8d85cbc4a20d55e3c704
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-06-05 13:00:58 +0530

    Time: 0 ms (100%), Space: 10.3 MB (81.31%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int t  = 0;
+        for(int i = 0; i<tickets.size(); i++){
+            if(i<=k) t += min(tickets[i], tickets[k]);
+            else t += min(tickets[i], tickets[k] - 1);
+        }
+    
+
+        return t;
+    }
+};
