3c0428a11a1a5af3fbdeb38b9394b72a146b52f5
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-06-08 12:03:45 +0530

    Time: 7 ms (62.22%), Space: 133.1 MB (68.32%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -3,16 +3,18 @@ public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        vector<int> v1, v2, v3;
+        vector<int> v1;
         
         for(int num : nums) {
             if(num < pivot) v1.push_back(num);
-            else if(num == pivot) v2.push_back(num);
-            else v3.push_back(num);
+        }
+        for(int num : nums) {
+            if(num == pivot) v1.push_back(num);
+        }
+        for(int num : nums) {
+            if(num > pivot) v1.push_back(num);
         }
 
-        v1.insert(v1.end(), v2.begin(), v2.end());
-        v1.insert(v1.end(), v3.begin(), v3.end());
         
         return v1;
     }
