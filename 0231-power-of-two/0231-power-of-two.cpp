145bab5282814d318d1bbe47cd99bd2d39b3e798
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-06-28 17:36:59 +0530

    Time: 0 ms (100%), Space: 7.9 MB (11.92%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,13 +3,8 @@ public:
     bool isPowerOfTwo(int n) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        if(n<=0) return false;
-        while(n>0){
-            if(n==1) return true;
-            if(n%2!=0) return false;
-            n/=2;
-        }
+        if(n>0 && (n & n-1)==0) return true;
 
-        return true;
+        return false;
     }
 };
