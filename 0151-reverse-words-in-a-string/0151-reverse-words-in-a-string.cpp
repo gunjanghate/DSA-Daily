c3c00ba9caf11d3ca1958a699bd411341309e4dd
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-06-30 12:41:29 +0530

    Time: 4 ms (22.54%), Space: 23.6 MB (13.49%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        reverse(s.begin(), s.end());
-        int n = s.size();
-        int left = 0;
-        int right = 0;
-        int i = 0;
-        while (i < n) {
-            while (i < n && s[i] == ' ')
-                i++;
-            if (i == n)
-                break;
-            while (i < n && s[i] != ' ') {
-                s[right++] = s[i++];
-            }
-            reverse(s.begin() + left, s.begin() + right);
-            s[right++] = ' ';
-            left = right;
-            i++;
+        stringstream ss(s);
+        string word;
+        string ans = "";
+
+        while (ss >> word) {
+            if (ans.empty())
+                ans = word;
+            else
+                ans = word + " " + ans;
         }
-        s.resize(right - 1);
-        return s;
+
+        return ans;
     }
 };
