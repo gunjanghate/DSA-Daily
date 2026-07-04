1bc4e5412220398ce37113195b545c5ad426960f
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-07-04 21:17:28 +0530

    Time: 190 ms (5.04%), Space: 81 MB (5.21%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,4 +1,5 @@
 class Solution {
+
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
 
@@ -6,54 +7,40 @@ public:
         int m = grid[0].size();
 
         vector<vector<int>> best(n, vector<int>(m, -1));
-
+        vector<pair<int, int>> dr = {{1, 0}, {-1, 0}, {0,1}, {0, -1}};
+    
         queue<vector<int>> q;
+        int sH = health - grid[0][0];
+        if(sH<1) return false;
+        q.push({0,0, sH});
+        best[0][0] = sH;
 
-        int startHealth = health - grid[0][0];
-
-        if (startHealth < 1)
-            return false;
-
-        q.push({0, 0, startHealth});
-        best[0][0] = startHealth;
-
-        vector<pair<int,int>> dir = {
-            {1,0}, {-1,0}, {0,1}, {0,-1}
-        };
-
-        while (!q.empty()) {
-
+        while(!q.empty()){
             auto curr = q.front();
             q.pop();
-
             int i = curr[0];
             int j = curr[1];
             int h = curr[2];
+            if(i==n-1 && j==m-1) return true; 
 
-            if (i == n - 1 && j == m - 1)
-                return true;
+            for(auto [dx, dy] : dr){
+                int nx = i + dx;
+                int ny = j + dy;
 
-            for (auto [dx, dy] : dir) {
+                if(nx < 0 ||  ny >= m || nx >= n || ny < 0) continue;
 
-                int ni = i + dx;
-                int nj = j + dy;
+                int nH = h - grid[nx][ny];
+                if(nH<1) continue;
+                if(best[nx][ny]>=nH) continue;
 
-                if (ni < 0 || nj < 0 || ni >= n || nj >= m)
-                    continue;
-
-                int newHealth = h - grid[ni][nj];
-
-                if (newHealth < 1)
-                    continue;
-
-                if (best[ni][nj] >= newHealth)
-                    continue;
-
-                best[ni][nj] = newHealth;
-                q.push({ni, nj, newHealth});
+                q.push({nx, ny, nH});
+                best[nx][ny] = nH;
             }
+
+
         }
 
-        return false;
+    return false;    
+
     }
 };
