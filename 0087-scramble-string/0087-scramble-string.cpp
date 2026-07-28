6e386533c2f3a5b1b22224ef61a6deaf5ffda52d
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-07-28 13:54:48 +0530

    Time: 15 ms (62.02%), Space: 16.8 MB (63.28%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    unordered_map<string, bool> dp;
+
+    bool solve(string s1, string s2) {
+
+        string key = s1 + "#" + s2;
+
+        if(dp.count(key))
+            return dp[key];
+
+        if(s1 == s2)
+            return dp[key] = true;
+
+        if(s1.length() != s2.length())
+            return dp[key] = false;
+
+        string a = s1, b = s2;
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        if(a != b)
+            return dp[key] = false;
+
+        int n = s1.size();
+
+        for(int i = 1; i < n; i++) {
+
+            // No swap
+            if(solve(s1.substr(0, i), s2.substr(0, i)) &&
+               solve(s1.substr(i), s2.substr(i)))
+                return dp[key] = true;
+
+            // Swap
+            if(solve(s1.substr(0, i), s2.substr(n - i)) &&
+               solve(s1.substr(i), s2.substr(0, n - i)))
+                return dp[key] = true;
+        }
+
+        return dp[key] = false;
+    }
+
+    bool isScramble(string s1, string s2) {
+        return solve(s1, s2);
+    }
+};
