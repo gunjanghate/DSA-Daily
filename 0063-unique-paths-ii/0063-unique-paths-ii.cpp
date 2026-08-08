ab078ebfbb6fd887e75e4dda229e8b4d9e005494
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-08-08 21:20:01 +0530

    Time: 0 ms (100%), Space: 12 MB (32.33%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -16,6 +16,26 @@ public:
         int m = o.size();
         int n = o[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(m, n, o, 0, 0, dp);
+        if(o[0][0]==1) return 0;
+
+        dp[0][0] = 1;
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(i==0 && j==0) continue;
+                if(o[i][j]==1){
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                int up = 0;
+                if(i>0) up += dp[i-1][j];
+                int left = 0;
+                if(j>0) left += dp[i][j-1];
+
+                dp[i][j] = up + left;
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
