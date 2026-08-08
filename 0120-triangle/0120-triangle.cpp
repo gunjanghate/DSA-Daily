22d80ae8e626ea24e71db185e96a71353727e59b
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-08-09 01:32:28 +0530

    Time: 0 ms (100%), Space: 13.3 MB (45.78%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int dfs(int i, int j, int m, vector<vector<int>>& t, vector<vector<int>>& dp){
+        if(i==m-1) return t[i][j];
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int down = t[i][j] + dfs(i+1, j, m, t, dp);
+        int diagonal = t[i][j] + dfs(i+1, j+1, m, t, dp);
+
+        return dp[i][j] = min(down , diagonal);
+    }
+    int minimumTotal(vector<vector<int>>& t) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int m = t.size();
+
+        vector<vector<int>> dp(m);
+        for(int i = 0; i<m; i++){
+            dp[i] = vector<int>(t[i].size(),-1);
+        }
+        dp[0][0] = t[0][0];
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<t[i].size(); j++){
+                if(i==0 && j==0){
+                    continue;
+                }
+                else if(j==0){
+                    dp[i][j] = t[i][j] + dp[i-1][j];
+                }
+                else if(j==i){
+                    dp[i][j] = t[i][j] + dp[i-1][j-1]; 
+
+                } else{
+                int up = t[i][j] + dp[i-1][j]; 
+
+                int diagonal = t[i][j] + dp[i-1][j-1];
+
+                dp[i][j] = min(up, diagonal);
+                }
+            }
+        }
+
+        return *min_element(dp[m - 1].begin(), dp[m - 1].end());
+    }
+};
