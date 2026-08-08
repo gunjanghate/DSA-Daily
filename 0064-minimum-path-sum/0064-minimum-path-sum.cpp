958494f6842a0124bb96a7614f06b2e7b9299da1
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-08-08 20:54:48 +0530

    Time: 0 ms (100%), Space: 16.5 MB (16.97%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -15,6 +15,22 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n,0));
-        return solve(0,0,m,n,grid, dp);
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(i==0 && j==0) dp[i][j] = grid[0][0];
+                else{
+                    int up = grid[i][j];
+                    if(i>0) up += dp[i-1][j];
+                    else up += 1e9;
+                    int left = grid[i][j];
+                    if(j>0) left += dp[i][j-1];
+                    else left += 1e9;
+
+                    dp[i][j] = min(up, left);
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
