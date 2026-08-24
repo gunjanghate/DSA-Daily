0b754d49d7c2fc6b5982211d53a986fae0edac5b
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-08-25 01:49:25 +0530

    Time: 131 ms (56.28%), Space: 13.5 MB (86.93%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
-    bool solve(int i, int target, vector<int>& nums, vector<vector<int>>& dp){
-        if(target==0){
-            return true; 
+    bool solve(int i, int target, vector<int>& nums, vector<vector<int>>& dp) {
+        if (target == 0) {
+            return true;
         }
-        if(i==0) return nums[0] == target;
+        if (i == 0)
+            return nums[0] == target;
 
-        
-        if(dp[i][target]!=-1) return dp[i][target];
+        if (dp[i][target] != -1)
+            return dp[i][target];
 
-        bool noTake = solve(i-1, target, nums, dp);
+        bool noTake = solve(i - 1, target, nums, dp);
 
         bool take = false;
-        if(nums[i]<=target){
-            take = solve(i-1, target - nums[i], nums, dp);
-        }        
+        if (nums[i] <= target) {
+            take = solve(i - 1, target - nums[i], nums, dp);
+        }
 
         return dp[i][target] = noTake || take;
     }
     bool canPartition(vector<int>& nums) {
         int ts = accumulate(nums.begin(), nums.end(), 0);
-        int s = ts/2;
-        if(ts%2!=0) return false;
+        int sum = ts / 2;
+        if (ts % 2 != 0)
+            return false;
         int n = nums.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(s+1, -1));
+        // vector<vector<int>> dp(n + 1, vector<int>(s + 1, -1));
 
-        return solve(n-1, s, nums , dp);
+        // return solve(n-1, s, nums , dp);
+        vector<bool> prev(sum + 1, false);
+        prev[0] = true;
+
+        if(nums[0] < sum) prev[nums[0]] = true;
+        vector<bool> curr(sum + 1, false);
+        curr[0] = true;
+
+        for (int i = 1; i < n; i++) {
+            for (int s = 1; s <= sum; s++) {
+
+                bool noTake = prev[s];
+                bool take = false;
+                if (nums[i] <= s)
+                    take = prev[s - nums[i]];
+
+                curr[s] = noTake || take;
+            }
+            prev = curr;
+        }
+
+        return prev[sum];
     }
 };
