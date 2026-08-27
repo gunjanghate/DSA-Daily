c8b76d71fa37923148f1cfb8f384599041345299
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-08-27 12:34:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,26 +1,43 @@
 class Solution {
-  public:
-    int solve(int i, vector<int> &val, vector<int> &wt, int W, vector<vector<int>> &dp){
-        if(i>=val.size() || W<=0) return 0;
-        
-        if(dp[i][W]!=-1) return dp[i][W];
-        
-        int take = 0;
-        if(wt[i]<=W){
-            take = val[i] + solve(i+1, val, wt, W-wt[i], dp); 
-        }
-        int nottake = 0;
-        nottake = solve(i+1, val, wt, W, dp);
-        
-        
-        return dp[i][W] = max(take, nottake);
-    }
-    int knapsack(int W, vector<int> &val, vector<int> &wt) {
-        // code here
-        int n = val.size();
-        vector<vector<int>> dp(n, vector<int>(W+1,-1));
-        
-        
-        return solve(0, val, wt, W, dp);
-    }
+	public:
+	int solve(int n, vector<int> &val, vector<int> &wt, int W, vector<vector<int>> &dp) {
+		if (n <= 0 || W <= 0)
+			return 0;
+		
+		if (dp[n][W] != -1)
+			return dp[n][W];
+		
+		int take = 0;
+		if (wt[n - 1] <= W) {
+			take = val[n - 1] + solve(n - 1, val, wt, W - wt[n - 1], dp);
+		}
+		int nottake = solve(n - 1, val, wt, W, dp);
+		
+		return dp[n][W] = max(take, nottake);
+	}
+	int knapsack(int W, vector<int> &val, vector<int> &wt) {
+		// code here
+		int n = val.size();
+		vector<vector<int>> dp(n + 1, vector<int>(W + 1));
+		
+		vector<int> prev(W+1, 0);
+		
+		
+		for (int i = 1; i <= n; i++) {
+		    vector<int> curr(W+1, 0);
+			for (int j = W; j >= 1; j--) {
+				int take = 0;
+				int nottake = 0;
+				
+				if(wt[i-1] <= j){
+				    take = val[i-1] + prev[j - wt[i-1]];
+				}
+				nottake = prev[j];
+			    prev[j] = max(take, nottake);
+			}
+// 			prev = curr;
+		}
+		
+		return prev[W];
+	}
 };
