f2a20965906c67255ea5e6e443dbe03d0db8c238
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-09-21 20:42:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -8,41 +8,47 @@ class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
         vector<vector<pair<int, int>>> adj(V);
-
-        for (const auto &edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int wt = edge[2];
-
-            adj[u].push_back({v, wt});
-            adj[v].push_back({u, wt}); 
+        
+        for(auto& e : edges){
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+            
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
         }
-
-        // Min-heap storing {distance, node}
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
         vector<int> dist(V, INT_MAX);
-
-        dist[src] = 0;
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
         pq.push({0, src});
-
-        while (!pq.empty()) {
+        dist[src] = 0;
+        
+        while(!pq.empty()){
+            int node = pq.top().second; 
             int dis = pq.top().first;
-            int node = pq.top().second;
             pq.pop();
-
-            if (dis > dist[node]) continue;
-
-            for (auto &edge : adj[node]) {
-                int adjNode = edge.first;
-                int edgewt = edge.second;
-
-                if (dis + edgewt < dist[adjNode]) {
-                    dist[adjNode] = dis + edgewt;
-                    pq.push({dist[adjNode], adjNode});
+            
+            if(dist[node] <  dis) continue;
+            
+            for(auto a : adj[node]){
+                int adjNode = a.first;
+                int wt = a.second;
+                
+                if(dis + wt < dist[adjNode]){
+                    
+                
+                dist[adjNode] = dis + wt;
+                pq.push({dist[adjNode], adjNode});
                 }
+                
             }
+            
+            
         }
-
+        
+        
         return dist;
     }
 };
