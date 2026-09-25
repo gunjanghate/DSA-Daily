e0548337000d79e843b89e982b5ec1a4d4bd2c6b
Gunjan Ghate <150514809+gunjanghate@users.noreply.github.com>
2026-09-26 02:17:44 +0530

    Time: 25 ms (96.33%), Space: 53.4 MB (92.57%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,31 +1,61 @@
 class Solution {
 public:
-    bool isPalindrome(string s){
-        string org = s;
-        reverse(s.begin(), s.end());
-        return org == s;
-    }
-    void solve(string s, vector<string>& str,
-               vector<vector<string>>& ans) {
-        // base case
-        if (s.size() == 0) {
+
+    void solve(string &s, int index, vector<string>& str,
+               vector<vector<string>>& ans,
+               vector<vector<bool>>& palindrome) {
+
+        if (index == s.size()) {
             ans.push_back(str);
             return;
         }
-        // rec logic
-        for (int i = 0; i < s.size(); i++) {
-            string curr = s.substr(0, i+1);
-            if(isPalindrome(curr)){
+
+        for (int i = index; i < s.size(); i++) {
+
+            if (palindrome[index][i]) {
+
+                string curr = s.substr(index, i - index + 1);
+
                 str.push_back(curr);
-                solve(s.substr(i + 1), str, ans);
+
+                solve(s, i + 1, str, ans, palindrome);
+
                 str.pop_back();
             }
         }
     }
+
     vector<vector<string>> partition(string s) {
+
+        int n = s.length();
+
+        vector<vector<bool>> palindrome(
+            n, vector<bool>(n, false)
+        );
+
+        for (int i = 0; i < n; i++) {
+            palindrome[i][i] = true;
+        }
+
+        for (int len = 2; len <= n; len++) {
+
+            for (int i = 0; i + len <= n; i++) {
+
+                int j = i + len - 1;
+
+                if (s[i] == s[j] &&
+                    (len == 2 || palindrome[i + 1][j - 1])) {
+
+                    palindrome[i][j] = true;
+                }
+            }
+        }
+
         vector<string> str;
         vector<vector<string>> ans;
-        solve(s, str, ans);
+
+        solve(s, 0, str, ans, palindrome);
+
         return ans;
     }
 };
